19b86d576dd9a4e6c082d0b5a2259541a36eec04
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2026-04-16 10:03:10 +0200

    [UPDATE] Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,24 @@
 import type { Metadata } from "next";
+import { Jost, Open_Sans, Caveat } from "next/font/google";
 import "./globals.css";
+
+const jost = Jost({
+  subsets: ["latin", "latin-ext"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-jost",
+});
+
+const caveat = Caveat({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-caveat",
+});
+
+const openSans = Open_Sans({
+  subsets: ["latin", "latin-ext"],
+  weight: ["400", "600"],
+  variable: "--font-open-sans",
+});
 
 export const metadata: Metadata = {
   title: "Menu | Mr. Rollo",
@@ -12,7 +31,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pl" className="h-full">
+    <html
+      lang="pl"
+      className={`h-full ${jost.variable} ${openSans.variable} ${caveat.variable}`}
+    >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
